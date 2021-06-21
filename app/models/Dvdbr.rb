5d31b7ecb832ba5f164c0285a0e90ffa3e29ac37
wagonnetdelaloire <wagonnetdelaloire@gmail.com>
2021-06-21 11:17:46 +0200

    useless commit test

--- app/models/Dvdbr.rb
+++ app/models/Dvdbr.rb
@@ -2,9 +2,7 @@ module Spree
   class Dvdbr < Spree::Base
     extend FriendlyId
     include Spree::ProductScopes
-
     friendly_id :slug_candidates, use: :history
-
     acts_as_paranoid
   end
 end
